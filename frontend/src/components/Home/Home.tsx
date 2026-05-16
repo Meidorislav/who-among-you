49b04d8fb4dcb7939063b32fa3eea6552057d033
Vladislav Bakin <meidorislav@gmail.com>
2026-05-17 01:48:36 +0500

    fix: fix title for mobile

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react'
 import { useTranslation } from 'react-i18next'
 import styles from './Home.module.css'
 
@@ -10,18 +11,30 @@ type HomeProps = {
 export const Home = ({ name, onNameChange, onJoin }: HomeProps) => {
   const { t } = useTranslation()
   const title = t('brand')
+  const words = title.split(' ')
+  let letterIndex = 0
 
   return (
     <main className={styles.home}>
       <h1 className={styles.title}>
-        {title.split('').map((char, i) => (
-          <span
-            key={i}
-            className={styles.titleLetter}
-            style={{ animationDelay: `${i * 0.05}s` }}
-          >
-            {char === ' ' ? ' ' : char}
-          </span>
+        {words.map((word, wi) => (
+          <Fragment key={wi}>
+            <span className={styles.titleWord}>
+              {word.split('').map((char, ci) => {
+                const i = letterIndex++
+                return (
+                  <span
+                    key={ci}
+                    className={styles.titleLetter}
+                    style={{ animationDelay: `${i * 0.05}s` }}
+                  >
+                    {char}
+                  </span>
+                )
+              })}
+            </span>
+            {wi < words.length - 1 && ' '}
+          </Fragment>
         ))}
       </h1>
 
