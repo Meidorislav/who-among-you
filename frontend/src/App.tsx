bbec4e86236d5427c89070ed9af92e807948cdbd
Vladislav Bakin <meidorislav@gmail.com>
2026-05-16 19:57:01 +0500

    feat: footer fronted

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { Header } from './components/Header/Header'
 import { Home } from './components/Home/Home'
+import { Footer } from './components/Footer/Footer'
 
 function App() {
-
   return (
     <>
-      <Header/>
-      <Home/>
+      <Header />
+      <Home />
+      <Footer />
     </>
   )
 }
